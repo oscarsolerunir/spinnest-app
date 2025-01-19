5a2e4c5f68bfbdc168ad325a0e53a5deb03331df
Óscar Soler <oscarsolerpastor@yahoo.es>
2025-01-19 20:40:35 +0100

    Add Firebase App Variables for Deployment

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,36 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
-export {
-  collection,
-  doc,
-  addDoc,
-  getDoc,
-  setDoc,
-  getDocs,
-  query,
-  updateDoc,
-  deleteDoc,
-  where,
-  onSnapshot,
-  documentId
-} from 'firebase/firestore'
-export {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  GoogleAuthProvider,
-  signInWithPopup,
-  sendPasswordResetEmail,
-  fetchSignInMethodsForEmail,
-  sendEmailVerification
-} from 'firebase/auth'
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_PROJECT_ID + '.firebaseapp.com',
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_PROJECT_ID + '.appspot.com'
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
