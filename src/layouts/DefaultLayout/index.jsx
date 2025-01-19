0ef72b6305bc5190b0a4d8cc6a1a93c07529ceeb
Óscar Soler <oscarsolerpastor@yahoo.es>
2025-01-19 18:06:57 +0100

    Fix Common Components Routes

--- src/layouts/DefaultLayout/index.jsx
+++ src/layouts/DefaultLayout/index.jsx
@@ -1,5 +1,5 @@
-import Header from '../../components/common/Header'
-import Footer from '../../components/common/Footer'
+import Header from '../../components/Common/Header'
+import Footer from '../../components/Common/Footer'
 import { Main } from './styles'
 
 const DefaultLayout = ({ children }) => {
