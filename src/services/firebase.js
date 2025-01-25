84447a72f9f6e94ee0726696f958355c7469bfa4
Óscar Soler <oscarsolerpastor@yahoo.es>
2025-01-25 13:39:30 +0100

    Delete Firebase Variables Mock

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -22,12 +22,11 @@ import {
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN || 'mock_auth_domain',
-  projectId: import.meta.env.VITE_PROJECT_ID || 'mock_project_id',
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || 'mock_storage_bucket',
-  messagingSenderId:
-    import.meta.env.VITE_MESSAGING_SENDER_ID || 'mock_messaging_sender_id',
-  appId: import.meta.env.VITE_APP_ID || 'mock_app_id'
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
