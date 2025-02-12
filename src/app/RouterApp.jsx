cfd00fa28b657307a8a3ae06a75147f2298e8c3f
Óscar Soler <oscarsolerpastor@yahoo.es>
2025-02-13 00:45:34 +0100

    Added Album Context

--- src/app/RouterApp.jsx
+++ src/app/RouterApp.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { UserProvider } from '../providers/UserContext'
+import { AlbumsProvider } from '../providers/AlbumsContext'
 import { WishlistProvider } from '../providers/WishlistContext'
 import PrivateRoute from '../components/Common/PrivateRoute'
 import DefaultLayout from '../layouts/Default'
@@ -26,110 +27,112 @@ import WishlistPage from '../pages/WishlistPage'
 const RouterApp = () => {
   return (
     <UserProvider>
-      <WishlistProvider>
-        <Router>
-          <DefaultLayout>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/followers" element={<FollowersPage />} />
-              <Route path="/following" element={<FollowingPage />} />
-              <Route path="/user/:userId" element={<UserPage />} />
-              <Route path="/feed" element={<FeedPage />} />
-              <Route
-                path="/add-album"
-                element={
-                  <PrivateRoute>
-                    <AddAlbumPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/albums"
-                element={
-                  <PrivateRoute>
-                    <UserAlbumsPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route path="/album/:id" element={<AlbumDetailsPage />} />
-              <Route
-                path="/add-collection"
-                element={
-                  <PrivateRoute>
-                    <AddCollectionPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/edit-collection/:id"
-                element={
-                  <PrivateRoute>
-                    <EditCollection />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/collection/:id"
-                element={
-                  <PrivateRoute>
-                    <ViewCollectionPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/collections"
-                element={
-                  <PrivateRoute>
-                    <UserCollectionsPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route
-                path="/profile"
-                element={
-                  <PrivateRoute>
-                    <UserProfile />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/edit-profile"
-                element={
-                  <PrivateRoute>
-                    <EditProfile />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/messages"
-                element={
-                  <PrivateRoute>
-                    <UserConversationsPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/messages/:conversationId"
-                element={
-                  <PrivateRoute>
-                    <UserMessagesPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/wishlist"
-                element={
-                  <PrivateRoute>
-                    <WishlistPage />
-                  </PrivateRoute>
-                }
-              />
-            </Routes>
-          </DefaultLayout>
-        </Router>
-      </WishlistProvider>
+      <AlbumsProvider>
+        <WishlistProvider>
+          <Router>
+            <DefaultLayout>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/followers" element={<FollowersPage />} />
+                <Route path="/following" element={<FollowingPage />} />
+                <Route path="/user/:userId" element={<UserPage />} />
+                <Route path="/feed" element={<FeedPage />} />
+                <Route
+                  path="/add-album"
+                  element={
+                    <PrivateRoute>
+                      <AddAlbumPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/albums"
+                  element={
+                    <PrivateRoute>
+                      <UserAlbumsPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="/album/:id" element={<AlbumDetailsPage />} />
+                <Route
+                  path="/add-collection"
+                  element={
+                    <PrivateRoute>
+                      <AddCollectionPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/edit-collection/:id"
+                  element={
+                    <PrivateRoute>
+                      <EditCollection />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/collection/:id"
+                  element={
+                    <PrivateRoute>
+                      <ViewCollectionPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/collections"
+                  element={
+                    <PrivateRoute>
+                      <UserCollectionsPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <PrivateRoute>
+                      <UserProfile />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/edit-profile"
+                  element={
+                    <PrivateRoute>
+                      <EditProfile />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/messages"
+                  element={
+                    <PrivateRoute>
+                      <UserConversationsPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/messages/:conversationId"
+                  element={
+                    <PrivateRoute>
+                      <UserMessagesPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/wishlist"
+                  element={
+                    <PrivateRoute>
+                      <WishlistPage />
+                    </PrivateRoute>
+                  }
+                />
+              </Routes>
+            </DefaultLayout>
+          </Router>
+        </WishlistProvider>
+      </AlbumsProvider>
     </UserProvider>
   )
 }
