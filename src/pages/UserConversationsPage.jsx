e7a7561f43a032c7ee42684c9e1d280e5d7f31bc
Óscar Soler <oscarsolerpastor@yahoo.es>
2025-02-25 10:27:37 +0100

    Remove UserConversations Component

--- src/pages/UserConversationsPage.jsx
+++ src/pages/UserConversationsPage.jsx
@@ -1,10 +1,43 @@
-import UserConversations from '../components/Users/UserConversations'
+import { useConversations } from '../contexts/ConversationsContext'
+import { useNavigate } from 'react-router-dom'
+import { auth } from '../services/firebase'
 
 const UserConversationsPage = () => {
+  const { conversations, markAsRead } = useConversations()
+  const navigate = useNavigate()
+
+  const handleConversationClick = async conversationId => {
+    await markAsRead(conversationId)
+    navigate(`/messages/${conversationId}`)
+  }
+
   return (
     <div className="p-4">
       <h1 className="text-2xl font-medium mb-4">Mensajes</h1>
-      <UserConversations />
+      <div className="py-4">
+        {conversations.length > 0 ? (
+          <ul className="space-y-2">
+            {conversations.map(convo => (
+              <li
+                key={convo.id}
+                onClick={() => handleConversationClick(convo.id)}
+                className={`p-4 rounded-md bg-darkaccent hover:bg-dark cursor-pointer ${
+                  convo.readBy && convo.readBy.includes(auth.currentUser.uid)
+                    ? 'font-normal'
+                    : 'font-medium'
+                }`}
+              >
+                <strong>{convo.userName}</strong> - {convo.lastMessage} -{' '}
+                {convo.readBy && convo.readBy.includes(auth.currentUser.uid)
+                  ? 'Leído'
+                  : 'No leído'}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Aún no has recibido ningún mensaje.</p>
+        )}
+      </div>
     </div>
   )
 }
